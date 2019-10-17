a18dde773f422e3a9f0d269af30e046929bd3e1a
MarcoGlauser <marco.glauser@gmail.com>
2019-10-17 10:26:39 +0200

    add editing of exercise

---
+++ lib/ui/exercise/EditExercise.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+import 'package:workout_timer/models/Exercise.dart';
+import 'package:workout_timer/provider/DatabaseService.dart';
+import 'package:workout_timer/ui/screens/HomeScreen.dart';
+
+
+class EditExercise extends StatefulWidget {
+  final Exercise exercise;
+
+  const EditExercise({Key key, this.exercise}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _EditExerciseState(exercise);
+}
+
+
+class _EditExerciseState extends State<EditExercise>{
+  final _formKey = GlobalKey<FormState>();
+  final Exercise exercise;
+
+  _EditExerciseState(this.exercise);
+
+  @override
+  Widget build(BuildContext context) {
+    return Login(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Edit Exercise'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: TextFormField(
+                    autofocus: true,
+                    initialValue: exercise.name,
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Exercise Name'
+                    ),
+                    onSaved: (val) {
+                      setState(() => exercise.name = val);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: TextFormField(
+                    initialValue: exercise.duration.inSeconds.toString(),
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter a number';
+                      }
+                      return null;
+                    },
+                    inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: 'Duration in seconds'
+                    ),
+                    onSaved: (val) => setState(() => exercise.duration = Duration(seconds: int.parse(val))),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      final form = _formKey.currentState;
+                      if (form.validate()) {
+                        form.save();
+                        //workout.addExercise(_exercise);
+                        DatabaseService db = GetIt.instance.get<DatabaseService>();
+                        db.saveExercise(exercise);
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Text('Save'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/exercise/ExerciseListItem.dart
+++ lib/ui/exercise/ExerciseListItem.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:workout_timer/models/Exercise.dart';
 import 'package:workout_timer/provider/DatabaseService.dart';
+import 'package:workout_timer/ui/exercise/EditExercise.dart';
 
 class ExerciseListItem extends StatelessWidget {
   final Exercise exercise;
@@ -13,14 +14,35 @@ class ExerciseListItem extends StatelessWidget {
     return ListTile(
       title: Text(exercise.name),
       subtitle: Text(exercise.duration.inSeconds.toString() + " Seconds"),
-      trailing: IconButton(
-        icon: Icon(
-          Icons.delete,
-          color: Theme.of(context).textTheme.body1.color,
-        ),
-        onPressed: () {
-          GetIt.instance.get<DatabaseService>().deleteExercise(exercise);
-        },
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.edit,
+              color: Theme.of(context).textTheme.body1.color,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EditExercise(
+                    exercise: exercise,
+                  ),
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.delete,
+              color: Theme.of(context).textTheme.body1.color,
+            ),
+            onPressed: () {
+              GetIt.instance.get<DatabaseService>().deleteExercise(exercise);
+            },
+          ),
+        ],
       ),
     );
   }
